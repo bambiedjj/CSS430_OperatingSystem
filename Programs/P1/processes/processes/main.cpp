14475a306c1ad18b0a1d004abe8a19c91b9cbae1
bambiedjj <bonkatie@uw.edu>
2015-04-14 14:50:40 -0700

    Initial Commit

---
+++ Programs/P1/processes/processes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  processes
+//
+//  Created by DJJ on 4/14/15.
+//  Copyright (c) 2015 DJJ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
